5f1f5b0fe94a22dec29b97e4852aa98c5d830fa7
LuccaVG <112782142+LuccaVG@users.noreply.github.com>
2024-06-19 10:29:21 -0300

    Update PerfilComponent.js

--- src/components/PerfilComponent/PerfilComponent.js
+++ src/components/PerfilComponent/PerfilComponent.js
@@ -1,8 +1,7 @@
-import { TextInput } from "react-native";
-import styled from "styled-components";
-import { render } from "react-native-web";
-
-//criando, estilizando e exportando os componentes a serem usados na pagina perfil
+import React, { useState } from 'react';
+import { Button, View, Image } from 'react-native';
+import styled from 'styled-components';
+import CameraComponent from './CameraComponent';
 
 export const PerfilBackGround = styled.SafeAreaView`
     flex: 1;
@@ -11,13 +10,13 @@ export const PerfilBackGround = styled.SafeAreaView`
     width: 100%;
     height: 100%;
     overflow: 'visible';
-`
+`;
 
 export const Seta = styled.SafeAreaView`
     right: 180px;
     top: 22px;
     overflow: 'visible';
-`
+`;
 
 export const ImagemLinda = styled.SafeAreaView`
     height: 160px;
@@ -28,7 +27,10 @@ export const ImagemLinda = styled.SafeAreaView`
     align-items: 'center';
     position: absolute;
     overflow: 'visible';
-`
+`;
+
+// Other styled components...
+
 export const CameraWhiteBackGround = styled.TouchableOpacity`
     height: 35px;
     width: 35px;
@@ -38,13 +40,14 @@ export const CameraWhiteBackGround = styled.TouchableOpacity`
     left: 50px;
     align-items: 'center';
     overflow: 'visible';
-`
+`;
 
 export const ImagemCss = styled.SafeAreaView`
     left: 30px;
     top: 30px;
     overflow: 'visible';
-`
+`;
+
 export const BottomOnAHeader = styled.SafeAreaView`
     height: 546px;
     width: 426px;
@@ -54,7 +57,7 @@ export const BottomOnAHeader = styled.SafeAreaView`
     align-items: 'center';
     border-radius: 25px;
     overflow: 'visible';
-`
+`;
 
 export const NameInput = styled.SafeAreaView`
     height: 52px;
@@ -66,7 +69,7 @@ export const NameInput = styled.SafeAreaView`
     top: 25px;
     margin-bottom: 20px;
     overflow: 'visible';
-`
+`;
 
 export const RaInput = styled.SafeAreaView`
     height: 52px;
@@ -78,7 +81,7 @@ export const RaInput = styled.SafeAreaView`
     top: 25px;
     margin-bottom: 20px;
     overflow: 'visible';
-`
+`;
 
 export const TurmaInput = styled.SafeAreaView`
     height: 52px;
@@ -90,7 +93,7 @@ export const TurmaInput = styled.SafeAreaView`
     top: 25px;
     margin-bottom: 5px;
     overflow: 'visible';
-`
+`;
 
 export const EditarButton = styled.TouchableOpacity`
     height: 50px;
@@ -100,7 +103,7 @@ export const EditarButton = styled.TouchableOpacity`
     left: 92px;
     top: 45px;
     overflow: 'visible';
-`
+`;
 
 export const NomeText = styled.Text`
     font-size: 20px;
@@ -108,7 +111,7 @@ export const NomeText = styled.Text`
     top: 25px;
     color: #FFFFFF;
     overflow: 'visible';
-`
+`;
 
 export const RaText = styled.Text`
     font-size: 20px;
@@ -116,7 +119,7 @@ export const RaText = styled.Text`
     top: 25px;
     color: #FFFFFF;
     overflow: 'visible';
-`
+`;
 
 export const TurmaText = styled.Text`
     font-size: 20px;
@@ -124,7 +127,7 @@ export const TurmaText = styled.Text`
     top: 25px;
     color: #FFFFFF;
     overflow: 'visible';
-`
+`;
 
 export const EditarText = styled.Text`
     font-size: 24px;
@@ -132,7 +135,7 @@ export const EditarText = styled.Text`
     top: 10px;
     color: #FFFFFF;
     overflow: 'visible';
-`
+`;
 
 export const NameTextInput = styled.TextInput`
     font-size: 18px;
@@ -140,14 +143,15 @@ export const NameTextInput = styled.TextInput`
     top: 12px;
     color: #8400F2;
     overflow: 'visible';
-`
+`;
+
 export const RaTextInput = styled.TextInput`
     font-size: 18px;
     left: 16px;
     top: 12px;
     color: #8400F2;
     overflow: 'visible';
-`
+`;
 
 export const TurmaTextInput = styled.TextInput`
     font-size: 18px;
@@ -155,18 +159,39 @@ export const TurmaTextInput = styled.TextInput`
     top: 12px;
     color: #8400F2;
     overflow: 'visible';
-`
+`;
 
 export const Titulo = styled.Text`
     font-size: 26px;
     color: #8400F2;
     top: 150px;
     overflow: 'visible';
-`
+`;
 
 export const SubTitulo = styled.Text`
     font-size: 16px;
     color: #8400F2;
     top: 160px;
     overflow: 'visible';
-`
+`;
+
+export default function PerfilComponent() {
+    const [cameraVisible, setCameraVisible] = useState(false);
+    const [profilePicture, setProfilePicture] = useState(null);
+
+    const handlePictureTaken = (uri) => {
+        setProfilePicture(uri);
+        setCameraVisible(false);
+    };
+
+    return (
+        <PerfilBackGround>
+            <Button title="Open Camera" onPress={() => setCameraVisible(true)} />
+            <Seta>
+                {profilePicture ? (
+                    <Image source={{ uri: profilePicture }} style={{ width: 160, height: 160, borderRadius: 90 }} />
+                ) : (
+                    <ImagemLinda />
+                )}
+            </Seta>
+            <
